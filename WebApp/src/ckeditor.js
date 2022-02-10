679b1eca7d4ee56ec56ac02e5228406d75fc8985
Kazem Javadi <kazemjavadi@hotmail.com>
2022-02-11 03:21:16 +0330

    adding some new features to ckeditor

---
+++ WebApp/src/ckeditor.js
@@ -0,0 +1,92 @@
+﻿document.getElementById('Input_Card_Answer').hidden = true;
+
+
+import BalloonEditor from '@ckeditor/ckeditor5-editor-balloon/src/ballooneditor';
+import EssentialsPlugin from '@ckeditor/ckeditor5-essentials/src/essentials';
+import UploadAdapterPlugin from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
+import AutoformatPlugin from '@ckeditor/ckeditor5-autoformat/src/autoformat';
+import BoldPlugin from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import ItalicPlugin from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import BlockQuotePlugin from '@ckeditor/ckeditor5-block-quote/src/blockquote';
+import HeadingPlugin from '@ckeditor/ckeditor5-heading/src/heading';
+import ParagraphPlugin from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';     // <--- ADDED
+import List from '@ckeditor/ckeditor5-list/src/list';
+import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline';
+import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight';
+
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
+import Code from '@ckeditor/ckeditor5-basic-styles/src/code';
+import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript';
+import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript';
+
+BalloonEditor
+    .create(document.querySelector('#editor'), {
+        // The plugins are now passed directly to .create().
+        plugins: [
+            EssentialsPlugin,
+            AutoformatPlugin,
+            BoldPlugin,
+            ItalicPlugin,
+            BlockQuotePlugin,
+            HeadingPlugin,
+            ParagraphPlugin,
+            UploadAdapterPlugin,
+            Alignment,
+            List,
+            HorizontalLine,
+            Highlight,
+            Bold, Italic, Underline, Strikethrough, Code, Subscript, Superscript
+        ],
+
+        // So is the rest of the default configuration.
+        toolbar: [
+            'heading',
+            'bold',
+            'italic',
+            'underline', 'strikethrough', 'code', 'subscript', 'superscript',
+            'bulletedList',
+            'numberedList',
+            'blockQuote',
+            'undo',
+            'redo',
+            `alignment`,
+            'horizontalLine',
+            'highlight'
+        ],
+        image: {
+            toolbar: [
+                'imageStyle:inline',
+                'imageStyle:block',
+                'imageStyle:side',
+                '|',
+                'toggleImageCaption',
+                'imageTextAlternative'
+            ]
+        }
+    })
+    .then(editor => {
+        console.log(editor);
+    })
+    .catch(error => {
+        console.error(error);
+    });
+
+    //.create(document.querySelector('#editor'), {
+    //    plugins: [Alignment],     // <--- MODIFIED
+    //})
+    //.then(editor => {
+    //    console.log('Editor was initialized', editor);
+    //})
+    //.catch(error => {
+    //    console.error(error.stack);
+    //});
+
+document.querySelector('#submit').addEventListener('click', () => {
+    alert(document.getElementById('editor').innerHTML);
+    document.getElementById('Input_Card_Answer').value = document.getElementById('editor').innerHTML;
+}
+);
---
+++ WebApp/webpack.config.js
@@ -0,0 +1,67 @@
+﻿// Webpack uses this to work with directories
+const path = require('path');
+const CKEditorWebpackPlugin = require('@ckeditor/ckeditor5-dev-webpack-plugin');
+const { styles } = require('@ckeditor/ckeditor5-dev-utils');
+
+// This is the main configuration object.
+// Here, you write different options and tell Webpack what to do
+module.exports = {
+
+    // Path to your entry point. From this file Webpack will begin its work
+    entry: './src/index.js',
+
+    // Path and filename of your result bundle.
+    // Webpack will bundle all JavaScript into this file
+    output: {
+        path: path.resolve(__dirname, './wwwroot/js/'),
+        publicPath: '',
+        filename: 'bundle.js'
+    },
+    plugins: [
+        // ...
+
+        new CKEditorWebpackPlugin({
+            // See https://ckeditor.com/docs/ckeditor5/latest/features/ui-language.html
+            language: 'en'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/,
+                use: ['raw-loader']
+            },
+            {
+                test: /ckeditor5-[^/\\]+[/\\]theme[/\\].+\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                        options: {
+                            injectType: 'singletonStyleTag',
+                            attributes: {
+                                'data-cke': true
+                            }
+                        }
+                    },
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: styles.getPostCssConfig({
+                                themeImporter: {
+                                    themePath: require.resolve('@ckeditor/ckeditor5-theme-lark')
+                                },
+                                minify: true
+                            })
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    // Default mode for Webpack is production.
+    // Depending on mode Webpack will apply different things
+    // on the final bundle. For now, we don't need production's JavaScript 
+    // minifying and other things, so let's set mode to development
+    mode: 'development'
+};
