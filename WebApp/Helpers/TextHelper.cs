c13f9c36018478d8eb51096300e2b26134d33840
Kazem Javadi <kazemjavadi@hotmail.com>
2022-02-11 02:49:59 +0330

    adding npm and ckeditor to project

---
+++ WebApp/Helpers/TextHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace WebApp.Helpers
+{
+    public class TextHelper
+    {
+        public static string StripHTML(string input)
+        {
+            return Regex.Replace(input, "<.*?>", String.Empty);
+        }
+    }
+}
